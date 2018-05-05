ac767728fa9fa65e77b793a1c3708b024a1eca3c
尤赫 <youhe@youhedeMacBook-Pro.local>
2018-05-05 16:10:04 -0700

    importance level

--- Agency analysis shiny.R
+++ Agency analysis shiny.R
@@ -9,42 +9,53 @@ data.agency$Created.Hour=as.factor(data.agency$Created.Hour)
 data.agency$Level = as.factor(data.agency$Level)
 data.agency$Complaint.SubType = as.factor(data.agency$Complaint.SubType)
 NewYork=qmap("New York",maptype = "roadmap",color="bw")
+importance = levels(data.agency$Level)
 
-ui = navbarPage("New York City Noise Complaints Analysis 2017",
-                   tabPanel(titlePanel(title = "Agency Response Analysis",windowTitle = "New York City 311 Noise Complaints Analysis"),
-                            sidebarLayout(
-                              sidebarPanel(
-                                helpText("Choose the following message to display"),
-                                checkboxGroupInput(inputId = "Agency",
-                                                   label = "Choose a Responding Agency",
-                                                   choices = list("New York Police Department" = "NYPD",
-                                                                  "Economic Development Corporation"="EDC",
-                                                                  "Environmental" = "DEP"),
-                                                   selected = "NYPD"),
-                                checkboxGroupInput(inputId = "SubType",
-                                                   label = "Noise Subtype",
-                                                   choices = list("Commercial","Helicopter","House of Worship","Park",
-                                                                  "Residential","Street/Sidewalk","Vehicle","Others"),
-                                                   selected = "Commercial"),
-                                
-                                selectInput(inputId = "Weekday",
-                                            label = "Choose a variable to display",
-                                            choices = list("Sun",
-                                                           "Mon",
-                                                           "Tue",
-                                                           "Wed",
-                                                           "Thur",
-                                                           "Fri",
-                                                           "Sat"))),
-                              mainPanel(
-                                plotOutput(outputId = "map")
-                              )
-                              
-                            )
-                            
-                            ),
-                   tabPanel("Overview")
-                            )
+ui = fluidPage(tabsetPanel(
+  ## First Tab
+  tabPanel("Overview"),
+  ## He YOU's tab 1 
+  tabPanel("Agency Response Analysis",
+    titlePanel("Complaint Importance Analysis",
+               windowTitle = "New York 311 Noise Complaint"),
+    sidebarPanel(
+      helpText("Choose the following message to display"),
+      checkboxGroupInput(inputId = "Agency",
+                         label = "Choose a Responding Agency",
+                         choices = list("New York Police Department" = "NYPD",
+                                        "Economic Development Corporation"="EDC",
+                                        "Environmental" = "DEP"),
+                         selected = "NYPD"),
+      checkboxGroupInput(inputId = "SubType",
+                         label = "Noise Subtype",
+                         choices = list("Commercial","Helicopter","House of Worship","Park",
+                                        "Residential","Street/Sidewalk","Vehicle","Others"),
+                         selected = "Commercial"),
+      selectInput(inputId = "Weekday",
+                  label = "Choose a weekday to display",
+                  choices = list("Sun",
+                                 "Mon",
+                                 "Tue",
+                                 "Wed",
+                                 "Thur",
+                                 "Fri",
+                                 "Sat")),
+      checkboxGroupInput(inputId = "Hour",
+                         label = "Choose created hour to display",
+                         choices = list("0","1","2","3","4","5","6","7","8","9","10",
+                                        "11","12","13","14","15","16","17","18","19",
+                                        "20","21","22","23"),
+                         selected = "0"),
+      checkboxGroupInput(inputId = "Level",
+                         label = "Choose importance level to display",
+                         choices = list("Level 1:Response or investigation demanded"="Level_1",
+                                        "Level 2:Solved or closed via referring or contacting"="Level_2",
+                                        "Level 3:Response unnecessary"="Level_3"),
+                         selected = "Level_1")
+      ),
+    mainPanel(
+      plotOutput(outputId = "map")))
+  ))
                      
 
 
@@ -52,17 +63,17 @@ ui = navbarPage("New York City Noise Complaints Analysis 2017",
 server=function(input,output) {
   dataset=reactive({
     data.agency %>%
-      filter(Agency%in%input$Agency,Created.Weekday==input$Weekday,Complaint.SubType==input$SubType)
+      filter(Agency%in%input$Agency,Created.Weekday==input$Weekday,Complaint.SubType==input$SubType,
+             Created.Hour%in%input$Hour,Level%in%input$Level)
   })
   
   
   
   output$map=renderPlot({
+    #fill_vector=input$Level
     NewYork+
-      stat_density2d(data=dataset(),aes(x=Longitude, y = Latitude,fill=..level.., alpha=..level..),bins=5,
-                     geom = "polygon") + 
-      facet_wrap(~Level)
-      scale_fill_gradient(low = "black",high = "red") +
+      stat_density2d(data=dataset(),aes(x=Longitude, y = Latitude,fill=Level, alpha=..level..),bins=5,
+                     geom = "polygon") +
       theme(legend.position = 'none')
     
   })
